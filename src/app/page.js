1bde022c5140b989cb514021d736878e793e1195
Clatson <124655279+clatsonin@users.noreply.github.com>
2023-07-14 13:28:52 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,67 @@
 "use client"
-import { useState } from "react";
-import { Configuration, OpenAIApi } from "openai";
+import { useState } from 'react';
 
-export default function Home() {
-  const [imageUrl, setImageUrl] = useState("");
+const ImageEdit = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [editedImageUrl, setEditedImageUrl] = useState('');
 
-  const createImg = async () => {
-    const configuration = new Configuration({
-      apiKey:process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = async (event) => {
+      const imageBuffer = event.target.result;
+      setImageUrl(URL.createObjectURL(file));
+      await editImage(imageBuffer);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  const editImage = async (imageBuffer) => {
+    const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY; // Replace with your actual API key
+    const endpoint = 'https://api.openai.com/v1/vision/davinci/edit';
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    };
+
+    const prompt = 'compare with android phone';
+    const n = 1;
+    const size = '256x256';
+
+    const body = JSON.stringify({
+      prompt,
+      n,
+      size
     });
-    const openai = new OpenAIApi(configuration);
 
-      const response = await openai.createImage({
-        prompt: "a white siamese cat",
-        n: 1,
-        size: "256x256",
+    try {
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers,
+        body
       });
 
-      setImageUrl(response.data.data[0].url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch');
+      }
 
+      const data = await response.json();
+      const imageUrl = data.output_url;
+      setEditedImageUrl(imageUrl);
+    } catch (error) {
+      console.error('Error occurred during image editing:', error);
+    }
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>image url:{imageUrl}</div>
-   <img src={imageUrl} width={1000} height={1000}/>
-      <button onClick={createImg}><div>click me</div></button>
-    </main>
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {imageUrl && <img src={imageUrl} alt="Original" />}
+      {editedImageUrl && <img src={editedImageUrl} alt="Edited" />}
+    </div>
   );
-}
+};
+
+export default ImageEdit;
